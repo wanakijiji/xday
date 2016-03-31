acad04049df0ad5e80bdd9b0ae54f1422d0188ee
Wana Kijiji <aroundit11@gmail.com>
2016-03-31 19:20:27 +0900

    Initial files

---
+++ xday.js
@@ -0,0 +1,24 @@
+/*
+ * 指定の日付で表示するコンテンツを切り替えるjQueryプラグイン
+ * ----------------------------------------------
+ */
+
+$( function() {
+	'use strict';
+
+  $.fn.xday = function( date ) {
+
+		// 日付の取得
+		var today = new Date(); // 今日
+		var xday = new Date( date ); // Xデー
+		// Xデー以降にコンテンツを表示
+		if( today > xday ){
+			$('.xday-before').hide();
+			$('.xday-after').show();
+		}
+
+		return this;
+
+	};
+
+});
