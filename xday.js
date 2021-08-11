dd10f45089b35cf2cf5808b439447457e3017edd
scitec2021 <80805704+scitec2021@users.noreply.github.com>
2021-08-11 11:21:10 +0900

    test2

--- xday.js
+++ xday.js
@@ -5,15 +5,15 @@
 
 $( function() {
 
-  $.fn.xday = function( date ) {
+  $.fn.xday = function(date) {
 
 		// 日付の取得
-		var today = new Date(); // 今日
-		var xday = new Date( date ); // Xデー
+		var today = new Date() // 今日
+		var xday = new Date(date) // Xデー
 		// Xデー以降にコンテンツを表示
 		if( today > xday ){
-			$('.xday-before').hide();
-			$('.xday-after').show();
+			$('.xday-before').hide()
+			$('.xday-after').show()
 		}
 
 		return this;
